396d3f03f45f90a3e7838d1d0f349960c355b1d1
timardex <timardex@gmail.com>
2019-12-18 22:02:29 +0100

    update classes

--- src/components/TheTable/NumbersSecondColumn.vue
+++ src/components/TheTable/NumbersSecondColumn.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="first-column columns">
+    <div class="second-column columns">
         <div v-for="item in number_second_column" :class="`form-check form-check-inline ${disabled_btn}`" :key="item.name">
             <label :class="`form-check-label number-${item.name}`" :for="`${item.id}`" :title="`${item.numbers}`">
                 <input class="form-check-input" type="checkbox" :id="`${item.id}`" :value="`${item.id}`"/>
--- src/components/TheTable/NumbersThirdColumn.vue
+++ src/components/TheTable/NumbersThirdColumn.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="first-column columns">
+    <div class="third-column columns">
         <div v-for="item in number_third_column" :class="`form-check form-check-inline ${disabled_btn}`" :key="item.name">
             <label :class="`form-check-label number-${item.name}`" :for="`${item.id}`" :title="`${item.numbers}`">
                 <input class="form-check-input" type="checkbox" :id="`${item.id}`" :value="`${item.id}`"/>
